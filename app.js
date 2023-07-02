04150cbd6d7f6e1fddd4e26524aef0b3e155dbe3
Nitya-vadoni <130901300+Nitya-vadoni@users.noreply.github.com>
2023-07-02 12:21:28 +0530

    Add all the files to github

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var http = require('http');
- http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.end('Hii KARAN');
- }).listen(3000);
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
